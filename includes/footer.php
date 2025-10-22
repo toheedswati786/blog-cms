33969b0410f2e5ba625f8a37281a1f83354615c4
Toheed Ahmad <toheedswati786@gmail.com>
2025-10-22 17:39:16 -0400

    first commit of uploading all the files and with editing some of the files

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<?php
+// includes/footer.php
+?>
+<footer class="site-footer">
+    <div class="container">
+        <p>&copy; <?php echo date('Y'); ?> Blog CMS. All rights reserved.</p>
+    </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<?php
+// includes/header.php
+?>
+<header class="site-header">
+    <div class="container">
+        <nav class="navbar">
+            <a href="<?php echo BASE_URL; ?>" class="logo">
+                <h1>Blog CMS</h1>
+            </a>
+            <ul class="nav-menu">
+                <li><a href="<?php echo BASE_URL; ?>">Home</a></li>
+                <li><a href="<?php echo BASE_URL; ?>blog-list.php">All Blogs</a></li>
+                <li><a href="<?php echo BASE_URL; ?>admin/index.php">Admin</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
